1860fde97cf6229a5e277af6334f7b28363a6706
unithmueller <85351534+unithmueller@users.noreply.github.com>
2023-08-18 15:39:10 +0200

    Started to introduce autotracking

---
+++ AdditionalScripts/automateSwiftTracking.m
@@ -0,0 +1,21 @@
+function automateSwiftTracking(LocalisationDataPath, expDispla, pBleach, ChangeMin, IterMax, Count, parameterPath)
+%Recursively call the swift tracking on the system to generate the correct
+%tracked files
+%
+
+
+%Create a save location
+
+%Prepare the settings file
+
+%Call the external swift function
+
+%Sort the new files into the new save location
+
+%Estimate the new properties
+
+%Calculate the change 
+
+%Return the data
+
+end
